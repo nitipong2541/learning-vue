60f875cb5d06d2a9095a2126f9a5647ea58c8d3d
nitipong2541 <nitipong2541.work@gmail.com>
2023-03-15 08:37:09 +0700

    edit signin and template

---
+++ src/pages/signin.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="p-4 border-gray-800 rounded-3xl drop-shadow-2xl">
+    <form>
+      <div class="p-2 text-2xl">Sign-In {{ username }}</div>
+      <div>
+        <label for="username" class="text-blue-800 px-1 text-lg">Username</label>
+        <input
+          type="text"
+          id="username"
+          v-model="username"
+          placeholder="Username"
+          :disabled="busySignIn"
+          class="px-2 py-1 border-2 border-gray-400 rounded-lg outline-black"
+          :class="{ 'bg-gray-400': busySignIn, 'text-gray-800': busySignIn }"
+        />
+      </div>
+      <div class="py-2">
+        <label for="password" class="text-blue-800 px-2 text-lg">Password</label>
+        <input
+          type="password"
+          id="password"
+          v-model="password"
+          placeholder="Password"
+          :disabled="busySignIn"
+          class="px-2 py-1 border-2 border-gray-400 rounded-lg outline-black"
+          :class="{ 'bg-gray-400': busySignIn, 'text-gray-800': busySignIn }"
+        />
+      </div>
+      <div class="flex">
+        <input type="checkbox" id="remember" v-model="remember" class="p-1" />
+        <label for="remember" class="p-2">Remember user</label>
+      </div>
+      <div>
+        <button
+          type="button"
+          :disabled="disableSignIn || busySignIn"
+          @click="SignInButton"
+          class="px-2 border-2 border-black rounded-2xl mr-4"
+          :class="[
+            /*busySignIn ? 'bg-gray-400' : 'bg-gray-200',*/
+            disableSignIn ? 'bg-gray-500' : 'bg-gray-100',
+          ]"
+        >
+          Sign-in
+        </button>
+        <button
+          type="reset"
+          @click="Reset"
+          class="px-3 border-2 border-black rounded-2xl bg-gray-100"
+        >
+          Reset
+        </button>
+      </div>
+      <div class="drop-shadow-2xl">
+        <button @click="PushTemplate">Go to /template</button>
+      </div>
+    </form>
+  </div>
+  <div
+    v-if="busySignIn"
+    class="fixed top-0 bottom-0 left-0 right-0 opacity-80 bg-gray-800"
+  >
+    <div class="text-white text-4xl pt-30">Please wait...</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      remember: false,
+      busySignIn: false,
+    };
+  },
+
+  computed: {
+    disableSignIn() {
+      return this.username === "" || this.password === "";
+    },
+  },
+  // created() {},
+  methods: {
+    SignInButton() {
+      this.busySignIn = true;
+      setTimeout(() => {
+        this.busySignIn = false;
+      }, 1500);
+    },
+    Reset() {
+      this.username = "";
+      this.password = "";
+    },
+    PushTemplate() {
+      this.$router.push("/template");
+    },
+  },
+};
+</script>
---
+++ src/pages/template.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="text-bold" :title="'Age ' + age">
+    Name : {{ name.toUpperCase() }} {{ lastname }}
+  </div>
+  <div>line 2</div>
+  <button :disabled="buttonDisable">Button</button>
+  <button @click="PushSignin">go to /signin</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Eman",
+      lastname: "Emantsal",
+      vbind: "Attribute Bindings",
+      age: 28,
+      buttonDisable: false,
+    };
+  },
+  methods: {
+    PushSignin() {
+      this.$router.push("/signin");
+    },
+  },
+};
+</script>
+
+<style>
+.text-bold {
+  font-weight: bold;
+  font-size: 30px;
+  font-style: italic;
+}
+</style>
